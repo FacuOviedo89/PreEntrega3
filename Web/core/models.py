53f35953b324c1d4638878e88324f6489c9e2c8b
Facu Oviedo89 <facu_cba89@hotmail.com>
2023-03-19 17:11:45 -0300

    inicialPage

---
+++ Web/core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    edad = models.IntegerField(max_length=99)
+
+    def __str__(self):
+        return f"{self.id} - {self.nombre} - {self.apellido} - {self.edad}"
+    
