7f038a68fd46439680c516e44b3215e80dfb60a7
Facu Oviedo89 <facu_cba89@hotmail.com>
2023-03-19 23:05:52 -0300

    modificating of Page

--- Web/core/models.py
+++ Web/core/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Persona(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
-    edad = models.IntegerField(max_length=99)
+    telefono = models.IntegerField(max_length=50)
 
     def __str__(self):
-        return f"{self.id} - {self.nombre} - {self.apellido} - {self.edad}"
+        return f"{self.id} - {self.nombre} - {self.apellido} - {self.telefono}"
     
